143913205cbec2f7aad73b3b8f1bca22f870df75
Petko <pavlovskipetko@abv.bg>
2021-11-23 08:52:57 +0200

    Initial

---
+++ migrations/2021-11-23-064717_anatomy/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+CREATE TABLE questions(
+      id SERIAL PRIMARY KEY,
+      label VARCHAR NOT NULL,
+      option_one VARCHAR NOT NULL,
+      option_two VARCHAR NOT NULL,
+      option_three VARCHAR NOT NULL,
+      option_four VARCHAR NOT NULL,
+      correct_answers VARCHAR NOT NULL
+);
